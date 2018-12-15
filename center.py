009781433acbc0ca3c437d166087da0cf9dbe39c
Louie Ayre <layre@college.harvard.edu>
2018-12-14 20:57:05 -0500

    filtered fix

--- center.py
+++ center.py
@@ -59,7 +59,6 @@ class Party:
 
         try:
             next_page_token = data["next_page_token"]
-            print next_page_token
         except:
             pass
 
@@ -112,9 +111,9 @@ print "PARTY 2\nPARTY 2\nPARTY 2\nPARTY 2\n"
 
 party1 = Party()
 print party1
-hakeem1 = User("Hakeem", 40.807835, -73.963957, 4, 0, "bar")
-louie1 = User("Louie", 40.709013, -74.013692, 4, 0, "restaurant")
-amadou1 = User("Amadou", 40.773585, -73.936027, 4, 0, "night_club")
+hakeem1 = User("Hakeem", 40.807835, -73.963957, 4, 5, ["bar"])
+louie1 = User("Louie", 40.709013, -74.013692, 4, 4, ["restaurant"])
+amadou1 = User("Amadou", 40.773585, -73.936027, 4, 4, ["night_club"])
 party1.addToParty(hakeem1)
 print party1
 party1.addToParty(louie1)
@@ -126,23 +125,43 @@ party1.findCenter()
 print party1.center
 
 for type in googleTypes.googleTypes:
-    party1.searchLocation(type, 500)
+    party1.searchLocation(type, 5000)
 
 print len(party1.places)
+party1.filterList()
+print "filtered", len(party1.filteredPlaces)
 
-def getDist(user, party, place):
-    return (m.sqrt((user.location[0]-place["location"][0])**2+(user.location[1]-place["location"][1])**2) - m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2))/m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2)
-
-def sadnessFunction(place, party):
-    weights = {"type" : 1, "price" : 1, "rating" : 1, "dist" : 1}
-    sadness = {}
-    for user in party.users:
-        sadness[user.name] = 0
-        dist = getDist(user, party, place)
-        if dist > 0:
-            sadness[user.name] += dist * weights["dist"]
-
-    print(sadness)
-
-
-sadnessFunction(party1.places[1], party1)
+count = 0
+for place in party1.filteredPlaces:
+    if place["price"] == None or place["rating"] == None:
+        count += 1
+print "count", count
+# def getDist(user, party, place):
+#     return (m.sqrt((user.location[0]-place["location"][0])**2+(user.location[1]-place["location"][1])**2) - m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2))/m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2)
+#
+# def sadnessFunction(place, party):
+#     weights = {"type" : 1, "price" : 1, "rating" : 1, "dist" : 1}
+#     sadness = {}
+#     print place
+#     for user in party.users:
+#         sadness[user.name] = 0
+#
+#         dist = getDist(user, party, place)
+#         if dist > 0:
+#             sadness[user.name] += dist * weights["dist"]
+#
+#         rating = user.ratingPref - place["rating"]
+#         if rating > 0:
+#             sadness[user.name] += rating * weights["rating"]
+#
+#         price = user.pricePref - place["price"]
+#         if price > 0:
+#             sadness[user.name] += price * weights["price"]
+#
+#         for event in user.eventPref:
+#             if event not in place["type"]
+#
+#     print(sadness)
+#
+#
+# sadnessFunction(party1.places[1], party1)
