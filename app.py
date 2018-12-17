dff6856d03522d4d14462ea6da254020bbc158a4
Hakeem Angulu <hakeem.angulu@gmail.com>
2018-12-17 18:45:58 -0500

    completed dynamic linking with logins

--- app.py
+++ app.py
@@ -44,6 +44,10 @@ def get_preferences(email):
 
 @app.route("/")
 def main():
+    """
+    Check if the user is logged in. If so, show them their dashboard. If not,
+    show them the home page.
+    """
     if "name" in session:
         return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
     else:
@@ -51,6 +55,10 @@ def main():
 
 @app.route("/signup")
 def sign_up():
+    """
+    Check if the user is logged in. If so, show them their dashboard. If not,
+    allow them to sign up.
+    """
     if "name" in session:
         return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
     else:
@@ -58,13 +66,22 @@ def sign_up():
 
 
 @app.route("/register", methods=["POST", "GET"])
-def register():
+def register(link=None):
+    """
+    Check if the user is logged in. If so, check if they are trying to join a
+    party and take them there. If not, take them to their dashboard
+    link (string): A link to a party being planned.
+    """
     if "name" in session:
-        return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
+        if link:
+            return redirect(url_for("plan_event", c_link=link))
+        else:
+            return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
     else:
         if request.method == "POST":
             c =  conn.cursor()
             try:
+                # Extract the data from the form
                 _name = request.form["inputName"]
                 _email = request.form["inputEmail"]
                 _password = request.form["inputPassword"]
@@ -83,7 +100,10 @@ def register():
                     c.close()
                     session["name"] = _name
                     session["email"] = _email
-                    return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
+                    if link:
+                        return redirect(url_for("plan_event", c_link=link))
+                    else:
+                        return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
                 else:
                     c.close()
                     return render_template("signup.html", error_msg="Please complete all required fields.")
@@ -96,7 +116,7 @@ def register():
             return render_template("signup.html")
 
 @app.route('/login', methods=["POST", "GET"])
-def login():
+def login(link=None):
     if "name" in session:
         return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
     else:
@@ -117,7 +137,10 @@ def login():
                                     _name = c.execute("SELECT name FROM users WHERE email = ?", (_email, )).fetchone()[0]
                                     session["name"] = _name
                                     session["email"] = _email
-                                    return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
+                                    if link:
+                                        return redirect(url_for("log_prefs", c_link=link))
+                                    else:
+                                        return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
                     c.close()
                     return render_template("login.html", error_msg="We were unable to find a match for these credentials. Please try again.")
                 else:
@@ -136,7 +159,7 @@ def logout():
     return render_template("index.html")
 
 @app.route("/pref", methods=["POST", "GET"])
-def log_prefs():
+def log_prefs(link=None):
     if request.method == "POST":
         c =  conn.cursor()
         try:
@@ -154,7 +177,10 @@ def log_prefs():
                 c.execute("UPDATE prefs SET activity = ?, price = ?, rating = ?, latitude = ?, longitude = ? WHERE email = ?", (_activity_type, _price_pref, _rating_pref, _lat_location, _long_location, _email))
                 conn.commit()
                 c.close()
-                return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=True)
+                if link:
+                    return redirect(url_for("plan_event", c_link=link))
+                else:
+                    return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=True)
             else:
                 c.close()
                 return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
@@ -195,53 +221,53 @@ def start_event():
 @app.route("/plan/<c_link>", methods=["POST", "GET"])
 def plan_event(c_link):
     if "name" not in session:
-        return render_template("dashboard.html", name=session["name"], prefs=get_preferences(session["email"]), recent_update=False)
-        return redirect(url_for("login"))
-    if request.method == "POST":
-        c =  conn.cursor()
-        try:
-            custom_link = c_link
-            named_gathering = c_link.split("-")[1]
-
-            _email = session["email"]
-            _name = session["name"]
-            _organizer = True
-
-            # Validate the input
-            if custom_link:
-                db_name = custom_link.replace("-", "")
-                create_query = "CREATE TABLE IF NOT EXISTS {} (email TEXT PRIMARY KEY, name TEXT, organizer BOOLEAN)".format(db_name)
-                upsert_query = "REPLACE INTO {} (email, name, organizer) VALUES (?, ?, ?)".format(db_name)
-                c.execute(create_query)
-                c.execute(upsert_query, (_email, _name, _organizer))
-
-                select_query = "SELECT * FROM {}".format(db_name)
-                raw_party = c.execute(select_query)
-                party = []
-
-                for user in raw_party.fetchall():
-                    raw_prefs = c.execute("SELECT activity, price, rating, latitude, longitude FROM prefs WHERE email = ?", (str(user[0]), )).fetchone()
-                    act_pref = str(raw_prefs[0]).split("\0")
-                    price_pref = str(raw_prefs[1])
-                    rating_pref = str(raw_prefs[2])
-                    latitude = str(raw_prefs[3])
-                    longitude = str(raw_prefs[4])
-
-                    user_dict = {"name": str(user[1]), "email": str(user[0]), "act_pref": act_pref, "price_pref": int(price_pref), "rating_pref": float(rating_pref), "location": (float(latitude), float(longitude))}
-
-                    party.append(user_dict)
-
-                return render_template("party.html", name=session["name"], named_gathering=named_gathering, link=custom_link, party=party)
-            else:
-                c.close()
-                return render_template("setup.html", name=session["name"])
-
-        except Exception as e:
-            return json.dumps({'error': str(e)})
-        finally:
-            c.close()
+        return redirect(url_for("login", link=c_link))
     else:
-        return render_template("setup.html", name=session["name"])
+        if request.method == "POST":
+            c =  conn.cursor()
+            try:
+                custom_link = c_link
+                named_gathering = c_link.split("-")[1]
+
+                _email = session["email"]
+                _name = session["name"]
+                _organizer = True
+
+                # Validate the input
+                if custom_link:
+                    db_name = custom_link.replace("-", "")
+                    create_query = "CREATE TABLE IF NOT EXISTS {} (email TEXT PRIMARY KEY, name TEXT, organizer BOOLEAN)".format(db_name)
+                    upsert_query = "REPLACE INTO {} (email, name, organizer) VALUES (?, ?, ?)".format(db_name)
+                    c.execute(create_query)
+                    c.execute(upsert_query, (_email, _name, _organizer))
+
+                    select_query = "SELECT * FROM {}".format(db_name)
+                    raw_party = c.execute(select_query)
+                    party = []
+
+                    for user in raw_party.fetchall():
+                        raw_prefs = c.execute("SELECT activity, price, rating, latitude, longitude FROM prefs WHERE email = ?", (str(user[0]), )).fetchone()
+                        act_pref = str(raw_prefs[0]).split("\0")
+                        price_pref = str(raw_prefs[1])
+                        rating_pref = str(raw_prefs[2])
+                        latitude = str(raw_prefs[3])
+                        longitude = str(raw_prefs[4])
+
+                        user_dict = {"name": str(user[1]), "email": str(user[0]), "act_pref": act_pref, "price_pref": int(price_pref), "rating_pref": float(rating_pref), "location": (float(latitude), float(longitude))}
+
+                        party.append(user_dict)
+
+                    return render_template("party.html", name=session["name"], named_gathering=named_gathering, link=custom_link, party=party)
+                else:
+                    c.close()
+                    return render_template("setup.html", name=session["name"])
+
+            except Exception as e:
+                return json.dumps({'error': str(e)})
+            finally:
+                c.close()
+        else:
+            return render_template("setup.html", name=session["name"])
 
 @app.route("/places/<c_link>", methods=["POST", "GET"])
 def solution(c_link):
