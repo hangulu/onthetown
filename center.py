2fd255dd981bd937e89105c228cbb1cfbec01c28
Hakeem Angulu <hakeem.angulu@gmail.com>
2018-12-16 19:45:09 -0500

    updated gitignore

--- center.py
+++ center.py
@@ -1,3 +1,12 @@
+"""
+This module implements the core classes of the social planning search problem:
+* User: a user of the program
+* Party: a group of users planning to go out together.
+
+It also implements the similarity function, which generates successors to a
+given plan by striving for a diversity of options.
+"""
+
 import config # make a config.py with an api_key from google places
 import googleTypes # a list of all the searcheable google types
 import math as m
@@ -11,12 +20,19 @@ requests_cache.install_cache('googleAPICache')
 
 class User:
     """
-    A user of the program
+    A user of the program.
     """
     def __init__(self, name, lat, lon, pricePref, ratingPref, eventPref):
         """
         Each user is initialized with their location and their preferences
         based on event, price, and ratings.
+        name (string): The user's name.
+        location (tuple): A tuple of latitude and longitude that encodes the
+        user's location.
+        organizer (boolean): Whether or not the user is the organizer.
+        pricePref (int): The user's price preference as an integer between 0 and 4, inclusive.
+        ratingPref (float): The user's preference for a place's rating as an float between 1.0 and 5.0, inclusive.
+        eventPref (string): The user's preference for a type of event. All options are in googleTypes.py.
         """
         self.name = name
         self.location = lat, lon
@@ -32,8 +48,11 @@ class Party:
     """
     def __init__(self):
         """
-        Party is initialized with the list of users at first if it is given,
-        otherwise it is null.
+        Party is initialized with the list of users at first, if given,
+        otherwise an empty list is used.
+        users (list): List of User objects.
+        center (tuple): Tuple of the center of all users' locations.
+        places (list): A list of potential places.
         """
         self.users = []
         self.center = None
@@ -41,8 +60,8 @@ class Party:
 
     def __str__(self):
         """
-        Outputs to improve the interactivity with the user, descriptors based
-        on the party size.
+        Outputs to improve the interactivity with the user, with descriptors
+        based on the party size.
         """
         output = ""
         if len(self.users) == 0:
@@ -66,43 +85,47 @@ class Party:
 
         for user in self.users:
             x += user.location[0]/len(self.users) # longitudes
-            y += user.location[1]/len(self.users)# latitudes
+            y += user.location[1]/len(self.users) # latitudes
 
         self.center = (x, y)
-        print "The center of the users is", self.center
+        print "The center of the users is: ", self.center
 
     def addToParty(self, user):
         """
-        The party is a list of users, so we append the user to the list
+        Add a user to the party.
+        user (User): A User object.
         """
+        # The first person in the party is the organizer
         if not self.users:
-            user.organizer = True # first person in the party is the organizer
+            user.organizer = True
             print user.name + " is the organizer of the party"
         self.users.append(user)
         print user.name + " has been added to the party"
-        self.findCenter() # as we add users we update the center.
+        # Update the center
+        self.findCenter()
 
     def searchLocation(self, type="", radius=5000):
         """
-        This is the function that takes the center location and searched for all
-        the possible events in the radius.
-
-        The type input is any one of the types from googleTypes.py. The radius
-        is dependent on how far we want to search out.
+        Take the center location and search for all possible events within a
+        given radius.
+        type (string): A type from googleTypes.py.
+        radius (int): How far to search from the center.
         """
-        APIKEY = config.api_key # created our own config files to protect api_key
-        # seting the parameters and making calls to the google places api
-        parameters = {"location": str(self.center[0])+","+str(self.center[1]),"radius":radius, "type":type,"key":APIKEY}
+        # Use config files to secure API keys
+        APIKEY = config.api_key
+
+        # Set the parameters and make calls to the Google Places API
+        parameters = {"location": str(self.center[0]) + "," + str(self.center[1]), "radius": radius, "type": type, "key": APIKEY}
         response = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=parameters)
         data = response.json()
 
         try:
-            # the initial limit of each call is 20, but if a result has a next
-            # page token we can get up to 60 results, but new calls have to be
-            # made, if there is no next page token, then it will just pass.
+            # The initial limit of each call is 20, but if a result has a next
+            # page token we can get up to 60 results. New calls have to be
+            # made, and if there is no next page token, continue.
             next_page_token = data["next_page_token"]
             while next_page_token:
-                parameters2 = {"pagetoken" : next_page_token, "key" : APIKEY}
+                parameters2 = {"pagetoken": next_page_token, "key": APIKEY}
                 response2 = requests.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json", params=parameters2)
                 data2 = response2.json()
                 try:
@@ -118,10 +141,11 @@ class Party:
 
     def addPlaces(self, data):
         """
-        Iterates through the response data recieved from API calls and adds the
-        places in our format to the list of all the places. We record the name,
-        price, rating, longitude and latitude, address, and types associated with
-        the event for each of the results.
+        Iterate through the response data recieved from API calls and add the
+        formatted places to the list of all the places. Record the name,
+        price, rating, longitude and latitude, address, and types associated
+        with the event for each of the results.
+        data (dict): The dictionary of Google Places API response data.
         """
         for place in data["results"]:
             name = place["name"] if "name" in place else None
@@ -131,7 +155,7 @@ class Party:
             address = place["vicinity"] if "vicinity" in place else None
             types = [str(type) for type in place["types"]] if "types" in place else None
 
-            # each place is its own dict for easy access to what you're looking for
+            # Each place is its own dict for easy access to key attributes
             dict = {"name": name,
                     "price": price,
                     "rating": rating,
@@ -144,20 +168,19 @@ class Party:
 
     def updatePlaces(self, radius = 5000):
         """
-        Iterates through all of the google types and searches the location fpr
-        each, to give all possible locations in the area.
+        Iterate through all of the Google types and searches the location for
+        each to give all possible locations in the area.
+        radius (int): How far to search from the center.
         """
         for type in googleTypes.googleTypes:
             self.searchLocation(type, radius)
 
     def filterList(self):
         """
-        Based on the inputs of each user for the preferences, this function looks
-        to see if an event in the total places list satisfies at least one persons
-        preferences and adds it to the list of filtered.
-
-        This allows us to remove unnecesary searches through events that would
-        provide no utility to any user.
+        Check if events in the list of total places satisfy at least one user's
+        preferences and add those events to a new list. This allows for the
+        removal of unnecesary searches through events that would provide no
+        utility to any user.
         """
         filteredList = []
         for user in self.users:
@@ -171,10 +194,8 @@ class Party:
 
     def updateAll(self):
         """
-        The updateAll function resets the places and repopulates it.
-
-        It is meant to be used upon adding a new person to the party where the
-        center changes.
+        Reset and repopulate the list of places. Used when adding a new person
+        to the party.
         """
         self.places = []
         rad = 0
@@ -184,74 +205,91 @@ class Party:
         self.filterList()
         self.assignSadness()
 
-    def getDist(self, user, party, place):
+    def getDist(self, user, place):
         """
-        Calculates the % distance that a user must travel to the location, vs.
-        the geographical center which is the most fair.
+        Compare the distance that a user must travel to the location to the
+        distance that a user must travel the geographical center. If this is
+        greater than 0, the user must travel more to go to the venue than they
+        would travel to go to the center.
+        user (User object): A user.
+        place (dict): A potential place.
 
-        If this is greater than 0, the user must travel more to go to go to the
-        venue than they would travel to go to the center.
+        return: Float of the difference between distances to the venue and
+        center.
         """
-        return (m.sqrt((user.location[0]-place["location"][0])**2+(user.location[1]-place["location"][1])**2) - m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2))/m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2)
+        dist_to_venue = m.sqrt((user.location[0] - place["location"][0]) ** 2 + (user.location[1] - place["location"][1]) ** 2)
+        dist_to_center = m.sqrt((user.location[0] - self.center[0]) ** 2 + (user.location[1] - self.center[1]) ** 2)
+        return dist_to_venue - dist_to_center
 
-    def sadnessFunction(self, place, party):
+    def sadnessFunction(self, place):
         """
-        Sadness function evaluates the how each person in the party is affected
-        by a specific event's type, price, rating, and distance, based on their preferences.
+        Evaluate how each user in the party is affected by a specific event's
+        type, price, rating, and distance, based on their preferences.
+        place (dict): A potential place.
 
-        The output is a list of the "sadness" rating for each user in the party.
+        return: List of the "sadness" incurred by a place for each user in the
+        party.
         """
-        # weights here are used to place emphasis on certain preferences more than others
-        weights = {"type" : 1., "price" : 1., "rating" : 1., "dist" : 1.}
-        sadness = [0] * len(party.users)
+        # weights is used to establish priorities among preferences
+        weights = {"type": 1., "price": 1., "rating": 1., "dist": 1.}
 
-        for i in range(len(party.users)):
-            dist = self.getDist(party.users[i], party, place)
+        # normalizers is used to normalize the sadness function to make its
+        # output more intuitive
+        normalizers = {"type": 0.504125555556, "price": 0.313995, "rating": 0.186832348864, "dist": 0.0940694970359}
+
+        # Initialize the sadness list
+        sadness = [0] * len(self.users)
+
+        # Iterate through the list of users and calculate their sadness
+        for i in range(len(self.users)):
+            # Distance
+            dist = self.getDist(self.users[i], place)
             if dist > 0:
-                sadness[i] += dist * weights["dist"]
+                sadness[i] += dist * weights["dist"] / normalizers["dist"]
 
-            rating = party.users[i].ratingPref - place["rating"]
+            # Rating
+            rating = self.users[i].ratingPref - place["rating"]
             if rating > 0:
-                sadness[i] += rating * weights["rating"]
+                sadness[i] += rating * weights["rating"] / normalizers["rating"]
 
-            price = float(place["price"] - party.users[i].pricePref)
+            # Price
+            price = float(place["price"] - self.users[i].pricePref)
             if price > 0:
-                sadness[i] += price * weights["price"]
+                sadness[i] += price * weights["price"] / normalizers["price"]
 
+            # Event type
             type = 0
-            for event in party.users[i].eventPref:
+            for event in self.users[i].eventPref:
                 if event not in place["types"]:
                     type += 1
-            if type == len(party.users[i].eventPref):
+            if type == len(self.users[i].eventPref):
                 type = 1
             else:
-                type = type/(2*len(party.users[i].eventPref))
-            sadness[i] += type
+                type = type / (2 * len(self.users[i].eventPref))
+            sadness[i] += type / normalizers["type"]
 
         return sadness
 
     def assignSadness(self):
         """
-        Assign a sadness to each event in the form of a list, where each person's
-        sadness is at an index of the list.
-
-        This makes it so that when we are searching through the tree, we do not
-        havee to evaluate at each step, the "values" will be there.
+        Assign a sadness list to each event. This makes it such that
+        evaluations of sadness are not unnecessarily repeated during search.
         """
         for place in self.filteredPlaces:
-            sadness = self.sadnessFunction(place, self)
+            sadness = self.sadnessFunction(place)
             place["sadness"] = sadness
 
 def similarity(event, places):
     """
-    This is a funtion that tests out the similarity of a given event to the
-    rest of the list and filters the events that are too similar, outputing
-    a list of events that are different the a selected event.
+    Return a list of dissimilar events to the given event. This is the
+    successor function used in the search tree: a node's successors are the
+    list of dissimilar options for the next choice.
+    event (dict): A potential place/event.
+    places (list): All potential places.
 
-    This is going to be used in the search tree, i.e. if a variable is chosen
-    we want to have a domain of dissimilar options for the next choice.
+    return: List of dissimilar places.
     """
-    newList = []
+    dissimilar = []
     for place in places:
         similarity = 0
         if place["price"] == event["price"]:
@@ -259,63 +297,9 @@ def similarity(event, places):
         for type1 in event["types"]:
             for type2 in place["types"]:
                 if type1 == type2:
-                    similarity += 1.0/float(len(place["types"]))
+                    similarity += 1.0 / float(len(place["types"]))
+        # 1.67 is the empirically generated threshold for similarity
+        # The empirical process behind this is described in the paper
         if similarity <= 1.67:
-            newList.append(place)
-    return newList
-
-
-# tests dont work together for some reason the second keeps data from the first but we can fix that later!
-# print "PARTY 1\nPARTY 1\nPARTY 1\nPARTY 1\n"
-# party = Party()
-# hakeem = User("Hakeem", 40.807835, -73.963957, 4, 5, ["bar", "restaurant"])
-# louie = User("Louie", 40.709013, -74.013692, 3, 4, ["restaurant", "movie"])
-# amadou = User("Amadou", 40.773585, -73.936027, 2, 3, ["night_club", "bar", "restaurant"])
-# party.addToParty(hakeem)
-# party.addToParty(louie)
-# party.addToParty(amadou)
-# party.updateAll()
-# print len(party.places)
-# print "filtered", len(party.filteredPlaces)
-
-
-print "PARTY 2\nPARTY 2\nPARTY 2\nPARTY 2\n"
-# test 2
-party1 = Party()
-hakeem1 = User("Hakeem", 40.807835, -73.963957, 4, 5, ["bar", "restaurant"])
-louie1 = User("Louie", 40.709013, -74.013692, 3, 4, ["restaurant", "movie"])
-amadou1 = User("Amadou", 40.773585, -73.936027, 2, 3, ["night_club", "bar", "restaurant"])
-hakeem2 = User("Hakeem", 40.807835, -73.963957, 1, 3, ["bar", "movie"])
-louie2 = User("Louie", 40.709013, -74.013692, 2, 4, ["restaurant", "bar"])
-amadou2 = User("Amadou", 40.773585, -73.936027, 3, 5, ["night_club", "bar"])
-party1.addToParty(hakeem1)
-party1.addToParty(louie1)
-party1.addToParty(amadou1)
-party1.addToParty(hakeem2)
-party1.addToParty(louie2)
-party1.addToParty(amadou2)
-party1.updateAll()
-
-print len(party1.places)
-print "filtered", len(party1.filteredPlaces)
-
-count = 0
-for place in party1.filteredPlaces:
-    if place["price"] == None or place["rating"] == None:
-        count += 1
-print "count", count
-
-# test for similarity fumction
-print "similarity function tests \n"
-list = party1.filteredPlaces
-randEvent = np.random.choice(list)
-newList = party1.similarity(randEvent, party1.filteredPlaces)
-for i in range(7):
-    print len(newList)
-    list = newList
-    randEvent = np.random.choice(list)
-    newList = party1.similarity(randEvent, list)
-
-# test for sadness function
-print "random sadness function tests \n"
-print party1.sadnessFunction(np.random.choice(party1.filteredPlaces), party1)
+            dissimilar.append(place)
+    return dissimilar
