145909a41643eb3713855887cfa1a8c7ff6e63b8
Hakeem Angulu <hakeem.angulu@gmail.com>
2018-12-18 20:29:20 -0500

    update documentation

--- sadnessDiagnostic.py
+++ sadnessDiagnostic.py
@@ -1,50 +1,74 @@
+"""
+This module implements the functions to run diagnostics on the sadness function
+and empirically determine weights.
+"""
+
 import center as c
 import math as m
 import numpy as np
 
-
-# Return additional travel percentage to chosen location relative to assigned c location
 def getDist(user, party, place):
-    return (m.sqrt((user.location[0]-place["location"][0])**2+(user.location[1]-place["location"][1])**2) - m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2))
+    """
+    Compare the distance that a user must travel to the location to the
+    distance that a user must travel the geographical center. If this is
+    greater than 0, the user must travel more to go to the venue than they
+    would travel to go to the center.
+    user (User object): A user.
+    party (Party object): A party.
+    place (dict): A potential place.
 
-# /m.sqrt((user.location[0]-party.center[0])**2+(user.location[1]-party.center[1])**2)
+    return: Float of the difference between distances to the venue and
+    center.
+    """
+    dist_to_venue = m.sqrt((user.location[0] - place["location"][0]) ** 2 + (user.location[1] - place["location"][1]) ** 2)
+    dist_to_center = m.sqrt((user.location[0] - party.center[0]) ** 2 + (user.location[1] - party.center[1]) ** 2)
+    return dist_to_venue - dist_to_center
+
+
+# Keep track of the sadness stemming from each of the 4 contributing factors
 error_count = 0
 def sadnessDiagnostic(party):
+    """
+    Run diagnostics on the sadnessFunction.
+    party (Party object): A given party.
+
+    return: a list of sadness scores.
+    """
+    # Get initital list of venues
     places = party.filteredPlaces
+
+    # Exit when list is empty
     if len(places) == 0:
-        print "SHIIIIIT SON"
         global error_count
         error_count += 1
-        return [0]*4
-    users = party.users
-    weights = {"dist" : 1., "rating" : 1., "price" : 1., "types" : 1.}
-    sadness = [0]*4
-    num = len(party.users)*len(places)
+        return [0] * 4
 
+    # Get list of users
+    users = party.users
+    # Option to view weighted output
+        # weights = {"dist" : 1./0.0156106747366, "rating" : 1./0.130747631926, "price" : 1./0.170200961101, "types" : 1./0.319765608129}
+    # Initialize list of sadness
+    sadness = [0] * 4
+
+    # Iterate through all the places
     for place in places:
         for i in range(len(users)):
+            # Get distance-attributed sadness
             dist = getDist(users[i], party, place)
             if dist > 0:
-                sadness[0] += (dist * weights["dist"])/0.0156106747366
-                #* (1/0.0940694970359)
-                 # * (1/0.0679126470653)
-                 # (0.09179504072728065)
+                sadness[0] += dist
 
-
+            # Get rating-attributed sadness
             rating = users[i].ratingPref - place["rating"]
             if rating > 0:
-                sadness[1] += (rating * weights["rating"])/0.130747631926
-                # * (1/0.186832348864)
-                 # * (1/0.109921338687)
-                 # (0.163095087303309)
+                sadness[1] += rating
 
+            # Get price-attributed sadness
             price = place["price"] - users[i].pricePref
             if price > 0:
-                sadness[2] += (price * weights["price"])/0.170200961101
-                # * (1/0.313995)
-                 # * (1/0.693561666667)
-                 # (0.3144504166681268)
+                sadness[2] += price
 
+            # Get type-attributed sadness
             type = 0
             for event in users[i].eventPref:
                 if event not in place["types"]:
@@ -53,51 +77,73 @@ def sadnessDiagnostic(party):
                 type = 1.
             else:
                 type = 0
-            sadness[3] += type * weights["types"]/0.319765608129
-            #* (1/0.504125555556)
-             # * (1/0.425713472222)
-             # (0.5319052430572467)
+            sadness[3] += type
 
-    sadness[0] = sadness[0]/num
-    sadness[1] = sadness[1]/num
-    sadness[2] = sadness[2]/num
-    sadness[3] = sadness[3]/num
+    # Get sadness per factor per user
+    num = len(party.users)*len(places)
+    sadness[0] = sadness[0] / num
+    sadness[1] = sadness[1] / num
+    sadness[2] = sadness[2] / num
+    sadness[3] = sadness[3] / num
 
     return sadness
-count = 0
+
+# Create party of specified length with random user preferences
 def randomParty(num):
-    coordlist = [(40.503780, -74.257985), (40.910901, -73.908623), (40.742613, -73.709127),
-                (40.597664, -73.763148), (40.612447, -74.090254), (40.770627, -73.958057),
-                (40.754135, -73.872459), (40.718946, -73.808422), (40.665516, -73.868019),
-                (40.653366, -73.970087), (40.661236, -73.742950), (40.560440, -73.920751)]
+    """
+    Generate a random party object of a certain size.
+    num (int): The size of the party.
+    """
+    # List of various coordinates in New York City
+    # Possible coordinates, in New York City.
+    coordlist = [(40.503780, -74.257985),
+                 (40.910901, -73.908623),
+                 (40.742613, -73.709127),
+                 (40.597664, -73.763148),
+                 (40.612447, -74.090254),
+                 (40.770627, -73.958057),
+                 (40.754135, -73.872459),
+                 (40.718946, -73.808422),
+                 (40.665516, -73.868019),
+                 (40.653366, -73.970087),
+                 (40.661236, -73.742950),
+                 (40.560440, -73.920751)]
+
+    # List of potential types
     typelist = ["bar", "restaurant", "night_club", "movie_theater", "cafe"]
+    # Initialize party
     party = c.Party()
+    # Initialize list to hold users coordinates
     coords = []
+    # For each user:
     for i in range(num):
+        # Get unique coordinates
         coord = coordlist[np.random.randint(0,len(coordlist))]
         while coord in coords:
             coord = coordlist[np.random.randint(0,len(coordlist))]
         coords.append(coord)
-    for i in range(num):
-        global count
-        count += 1
+        # Get unique username
         username = "User" + str(i)
-        coord = coords[np.random.randint(0,len(coords))]
+        # Get random rating, uniform continuous [2,5]
         rating = 2 + 3*np.random.random_sample()
+        # Get random price, uniform discrete [1,4]
         price = np.random.randint(1,5)
+        # Get random type preferences of size 1-3
         types = np.random.choice(typelist,size = np.random.randint(1,4),replace = False)
-        # for i in range(np.random.randint(1,4)):
-        #     types.append(np.random.choice(typelist))
+        # Create user
         user = c.User(username, coord[0], coord[1], price, rating, types)
+        # Add user to party
         party.addToParty(user)
+
     party.updateAll()
     return party
 
-total_sadness = [0]*4
+# Get average sadness attributed to factors over specified number of random parties
+total_sadness = [0] * 4
 pnum = 10
+# Run simulations
 for i in range(pnum):
     party = randomParty(np.random.randint(2,7))
-    print len(party.filteredPlaces)
     sadness = sadnessDiagnostic(party)
     print "Party", i, "User Count", len(party.users)
     print "Party", i, "Distance", sadness[0], "Rating", sadness[1], "Price", sadness[2], "Type", sadness[3]
@@ -105,19 +151,6 @@ for i in range(pnum):
     total_sadness[1] += sadness[1]
     total_sadness[2] += sadness[2]
     total_sadness[3] += sadness[3]
-print "Total User Count", count
 print "Total Error Count", error_count
-print "Total", "Distance", total_sadness[0]/(pnum - error_count), "Rating", total_sadness[1]/(pnum - error_count), "Price", total_sadness[2]/(pnum - error_count), "Type", total_sadness[3]/(pnum - error_count)
-
-# Total -- 50 -- Distance 0.0161592456178 Rating 0.127213537013 Price 0.169797451502 Type 0.457565392461
-# Total -- 20 -- Distance 0.0188080062544 Rating 0.138408560697 Price 0.173751310925 Type 0.279295517729
-# Total -- 500 -- Distance 0.0156106747366 Rating 0.130747631926 Price 0.170200961101 Type 0.319765608129
-    # Error count 3
-
-# Normalized testa
-# Total -- 10 -- Distance 1.09756140318 Rating 1.16887866081 Price 0.968105008073 Type 0.870719462553
-# Total -- 10 -- Distance 0.841315174016 Rating 1.69979152832 Price 0.656875960548 Type 0.958094400112
-# Total -- 10 -- Distance 1.16633972916 Rating 1.01652446126 Price 0.551033347781 Type 0.929086713049
-# Total -- 10 -- Distance 1.00351374524 Rating 1.04211459919 Price 0.879781636077 Type 1.09478562689
-# Total -- 10 -- Distance 0.785663224253 Rating 1.59192226143 Price 0.879333036906 Type 1.0029138118
-# Total -- 10 -- Distance 0.993086300076 Rating 0.811261024419 Price 1.05950515338 Type 0.621396677955
+# Return average over parties, but adjust for error_count
+print "Total", "Distance", total_sadness[0] / (pnum - error_count), "Rating", total_sadness[1] / (pnum - error_count), "Price", total_sadness[2] / (pnum - error_count), "Type", total_sadness[3] / (pnum - error_count)
